925e6d7362002feacf2cb9dd49cc42368ded09d0
pranjal-jamuar <jamuar64@gmail.com>
2020-03-15 18:25:25 +0530

    queue2

--- code.c
+++ code.c
@@ -1,80 +1,58 @@
-void Q1()  //Round Robin Algorithm
-	         {
-	            printf("\nRound Robin Algorithm:\n");
-	         	int i,a,n,m,s1=0,s2=0;
-	            int B[10],req[10],pid[10],wait[10],turn[10],arr[10],bur[10],TimeQuant=4;
-	            printf("Enter the number of Processes: ");
-	            scanf("%d",&n);
-	            int temp=n;
-	            for(i=0;i<n;i++)
+	void Q2()  // Priority Schedulling Algorithm
+	    {
+	        printf("\nPriority Scheduling Algorithm\n");
+	        int i,j,c,position,t,avg_wait,avg_turn;
+	        int Tot=0,bur1[20],p1[20],wait1[20],turn1[20],pr1[20];
+	        printf("Enter Total Number of Processes:");
+	        scanf("%d",&c);
+	        printf("\nEnter Burst Time and Priority:\n");
+	        for(i=0;i<c;i++)
+	        {
+	            printf("\nP[%d]\n",c+1);
+	            printf("The Burst Time is:");
+	            scanf("%d",&bur1[i]);
+	            printf("Priority is:");
+	            scanf("%d",&pr1[i]);
+	            p1[i]=c+1;           //It contains Process Number
+	        }
+	        //sorting burst time, priority and process number in ascending order using selection sort
+	        for(i=0;i<c;i++)
+	        {
+	            position=c;
+	            for(j=i+1;j<c;j++)
 	            {
-	                pid[i]=i;
-	                printf("\nThe Arrival time of the Process P%d ",i+1);
-	                scanf("%d",&arr[i]);
-	                printf("\nThe Burst time of the Process P%d ",i+1);
-	                scanf("%d",&bur[i]);
+	                if(pr1[j]<pr1[position])
+	                    position=j;
 	            }
-	            for(i=0;i<n;i++)
-	            {
-	                B[i]=bur[i];
-	            }
-	            int K=0;
-	            while(1)
-	            {
-	                int d=1;
-	                for(i=0;i<n;i++)
-	                {
-	                    if(B[i]>0)
-	                    {
-	                        d=0;
-	                        if(B[i]>TimeQuant)
-	                        {
-	                            K=K+TimeQuant;
-	                            B[i]=B[i]-TimeQuant;
-	                        }
-	                        else
-	                        {
-	                            K=K+B[i];
-	                            wait[i]=K-B[i];
-	                            B[i]=0;
-	                        }
-	                    }
-	                }
-	                if(d==1)
-	                {
-	                    break;
-	                }
-	            }
-	            for(i=0;i<n;i++)
-	            {
-	                if(wait[i]==0)
-	                {
-	                    req[i]=pid[i];
-	                }
-	            }
-	            printf("For Waiting time:\n");
-	            for(i=0;i<n;i++)
-	            {
-	                printf("P%d ",i+1);
-	                printf("%d\n",wait[i]);
-	            }
-	            for(i=0;i<n;i++)
-	            {
-	                turn[i]=bur[i]+wait[i];
-	            }
-	            printf("Turnaround Time\n");
-	            for(i=0;i<n;i++)
-	            {
-	                printf("P%d ",i+1);
-	                printf("%d\n",turn[i]);
-	            }
-	            for(i=0;i<n;i++)
-	            {
-	                s2=s2+wait[i];
-	                s1=s1+turn[i];
-	            }
-	            printf("The Average Waiting Time is:\n");
-	            printf("%f",(float)s2/(float)n);
-	            printf("\nThe Average Turnaround Time is:\n");
-	            printf("%f",(float)s1/(float)n);
-	}
+	            t=pr1[c];
+	            pr1[c]=pr1[position];
+	            pr1[position]=t;
+	            t=bur1[c];
+	            bur1[c]=bur1[position];
+	            bur1[position]=t;
+	            t=p1[c];
+	            p1[c]=p1[position];
+	            p1[position]=t;
+	        }
+	        wait1[0]=0;	//waiting time for first process is zero
+	        //calculate waiting time
+	        for(i=1;i<c;i++)
+	        {
+	            wait1[i]=0;
+	            for(j=0;j<i;j++)
+	                wait1[i]+=bur1[j];
+	            Tot+=wait1[i];
+	        }
+	        avg_wait=Tot/c;      //average waiting time
+	        Tot=0;
+	        printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+	        for(i=0;i<c;i++)
+	        {
+	            turn1[i]=bur1[i]+wait1[i];     //calculate turnaround time
+	            Tot+=turn1[i];
+	            printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p1[i],bur1[i],wait1[i],turn1[i]);
+	        }
+	        avg_turn=Tot/c;     //average turnaround time
+	        printf("\n\nAverage Waiting Time=%d",avg_wait);
+	        printf("\nAverage Turnaround Time=%d\n",avg_turn);
+	    }
